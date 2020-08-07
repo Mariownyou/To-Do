0d503238836005f00ec7a4d0ce2d2f5c4a386ec6
Leo Kondratev <39639000+Mariownyou@users.noreply.github.com>
2020-08-07 23:08:18 +0300

    done

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -131,4 +130,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
